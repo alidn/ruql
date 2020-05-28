d83adee46466fa29dc248b94b9813bff3dec8fbb
Ali <ali.nili128@gmail.com>
2020-05-27 21:01:21 -0700

    Added symbol-lexer and keyword-lexer and a few unit tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,8 +1,9 @@
-use std::fs::OpenOptions;
+use crate::lexer::TokenKind::Symbol;
 
 #[derive(PartialEq, Debug)]
 struct Token {
     value: String,
+    kind: TokenKind,
 }
 
 #[derive(Default)]
@@ -17,7 +18,187 @@ struct Cursor {
     loc: Location,
 }
 
-fn lex_delimiter(source: &str, delimiter: char) -> Option<(Token, Cursor)> {
+// /semicolonSymbol  symbol = ";"
+// asteriskSymbol   symbol = "*"
+// commaSymbol      symbol = ","
+// leftParenSymbol  symbol = "("
+// rightParenSymbol symbol = ")"
+// eqSymbol         symbol = "="
+// neqSymbol        symbol = "<>"
+// neqSymbol2       symbol = "!="
+// concatSymbol     symbol = "||"
+// plusSymbol       symbol = "+"
+// ltSymbol         symbol = "<"
+// lteSymbol        symbol = "<="
+// gtSymbol         symbol = ">"
+// gteSymbol        symbol = ">="
+#[derive(Clone, Debug, PartialEq)]
+enum SymbolType {
+    Semicolon,
+    Comma,
+    LeftParen,
+    RightParen,
+    Eq,
+    Neq,
+    Plus,
+    Lt,
+    Lte,
+    Gt,
+    Gte,
+    Endl,
+    Tab,
+}
+
+impl SymbolType {
+    pub fn value(&self) -> &str {
+        match self {
+            SymbolType::Semicolon => ";",
+            SymbolType::Comma => ",",
+            SymbolType::LeftParen => "(",
+            SymbolType::RightParen => ")",
+            SymbolType::Eq => "=",
+            SymbolType::Neq => "!=",
+            SymbolType::Plus => "+",
+            SymbolType::Lt => "<",
+            SymbolType::Lte => "<=",
+            SymbolType::Gt => ">",
+            SymbolType::Gte => ">=",
+            SymbolType::Endl => "\n",
+            SymbolType::Tab => "\t",
+        }
+    }
+
+    pub fn values() -> Vec<SymbolType> {
+        let symbolTypes = [
+            SymbolType::Semicolon,
+            SymbolType::Comma,
+            SymbolType::LeftParen,
+            SymbolType::RightParen,
+            SymbolType::Eq,
+            SymbolType::Neq,
+            SymbolType::Plus,
+            SymbolType::Lt,
+            SymbolType::Lte,
+            SymbolType::Gt,
+            SymbolType::Gte,
+            SymbolType::Endl,
+            SymbolType::Tab,
+        ];
+        symbolTypes.to_vec()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum KeywordType {
+    Select,
+    From,
+    Create,
+    Insert,
+    As,
+    Into,
+    Table,
+    Values,
+    Int,
+    Text,
+}
+
+impl KeywordType {
+    pub fn value(&self) -> &str {
+        match self {
+            KeywordType::Select => "select",
+            KeywordType::From => "from",
+            KeywordType::Create => "create",
+            KeywordType::Insert => "insert",
+            KeywordType::As => "as",
+            KeywordType::Table => "table",
+            KeywordType::Values => "values",
+            KeywordType::Int => "int",
+            KeywordType::Text => "text",
+            KeywordType::Into => "into",
+        }
+    }
+
+    pub fn values() -> Vec<KeywordType> {
+        let keywordTypes: Vec<KeywordType> = [
+            KeywordType::Select,
+            KeywordType::From,
+            KeywordType::Create,
+            KeywordType::Insert,
+            KeywordType::As,
+            KeywordType::Table,
+            KeywordType::Values,
+            KeywordType::Into,
+            KeywordType::Int,
+            KeywordType::Text,
+        ]
+        .to_vec();
+        keywordTypes
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum TokenKind {
+    Keyword(KeywordType),
+    Symbol(SymbolType),
+    Identifier,
+    String,
+    Numeric,
+    Bool,
+    Null,
+}
+
+fn lex_symbol(source: &str) -> Option<(Token, Cursor)> {
+    let mut cursor = Cursor::default();
+
+    // cases where the cursor should move to the next line.
+    if source.starts_with(SymbolType::Endl.value()) || source.starts_with(SymbolType::Tab.value()) {
+        cursor.loc.line += 1;
+        cursor.loc.column = 0;
+    }
+
+    for symbol in SymbolType::values() {
+        if source.starts_with(symbol.value()) {
+            cursor.pointer = symbol.value().len();
+            cursor.loc.column = symbol.value().len();
+            return Some((
+                Token {
+                    value: symbol.value().to_string(),
+                    kind: TokenKind::Symbol(symbol),
+                },
+                cursor,
+            ));
+        }
+    }
+
+    None
+}
+
+// TODO: fix case sensitivity.
+fn lex_keyword(source: &str) -> Option<(Token, Cursor)> {
+    let keyword_match = String::default();
+    for keyword in KeywordType::values().iter() {
+        if source.starts_with(keyword.value()) {
+            let mut cursor = Cursor::default();
+            cursor.pointer = keyword.value().len();
+            cursor.loc.column = cursor.pointer;
+            return Some((
+                Token {
+                    value: keyword.value().to_string(),
+                    kind: TokenKind::Keyword(keyword.clone()),
+                },
+                cursor,
+            ));
+        }
+    }
+
+    None
+}
+
+fn lex_string(source: &str) -> Option<(Token, Cursor)> {
+    lex_char_delimited(source, '\'')
+}
+
+fn lex_char_delimited(source: &str, delimiter: char) -> Option<(Token, Cursor)> {
     let mut source_iterator = source.char_indices();
     let mut cursor = Cursor::default();
 
@@ -43,12 +224,14 @@ fn lex_delimiter(source: &str, delimiter: char) -> Option<(Token, Cursor)> {
         }
     }
 
+    // TODO: this might be wrong.
     cursor.pointer = last_index + 1;
     cursor.loc.column = last_index + 1;
 
     Some((
         Token {
             value: source[..last_index].to_string(),
+            kind: TokenKind::String,
         },
         cursor,
     ))
@@ -106,6 +289,7 @@ fn lex_numeric(source: &str) -> Option<(Token, Cursor)> {
     Some((
         Token {
             value: source[..cursor.pointer].to_string(),
+            kind: TokenKind::Numeric,
         },
         cursor,
     ))
@@ -113,7 +297,11 @@ fn lex_numeric(source: &str) -> Option<(Token, Cursor)> {
 
 #[cfg(test)]
 mod tests {
-    use crate::lexer::{lex_delimiter, lex_numeric};
+    use crate::lexer::TokenKind::Keyword;
+    use crate::lexer::{
+        lex_char_delimited, lex_keyword, lex_numeric, lex_string, lex_symbol, KeywordType,
+        SymbolType, TokenKind,
+    };
 
     fn test_numeric_lexer(source: &str, should_be_none: bool, expected_result: &str) {
         let received_token = lex_numeric(source);
@@ -132,7 +320,7 @@ mod tests {
     }
 
     #[test]
-    fn trest_lex_numeric_basic_number_one_digit() {
+    fn test_lex_numeric_basic_number_one_digit() {
         let source = "8";
         test_numeric_lexer(source, false, source);
     }
@@ -224,7 +412,7 @@ mod tests {
     #[test]
     fn test_lex_delimiter_basic() {
         let source = "'aabbcc'";
-        let result = lex_delimiter(source, '\'');
+        let result = lex_char_delimited(source, '\'');
         assert!(result.is_some());
         if let Some((token, _)) = result {
             assert_eq!(token.value, source);
@@ -234,21 +422,21 @@ mod tests {
     #[test]
     fn test_lex_delimiter_no_end() {
         let source = "'aabb";
-        let result = lex_delimiter(source, '\'');
+        let result = lex_char_delimited(source, '\'');
         assert!(result.is_none());
     }
 
     #[test]
     fn test_lex_delimiter_no_start() {
         let source = "asdf'";
-        let result = lex_delimiter(source, '\'');
+        let result = lex_char_delimited(source, '\'');
         assert!(result.is_none());
     }
 
     #[test]
     fn test_lex_delimiter_escape() {
         let source = "'asdf''";
-        let result = lex_delimiter(source, '\'');
+        let result = lex_char_delimited(source, '\'');
         assert!(result.is_some());
         if let Some((token, _)) = result {
             assert_eq!(token.value, source);
@@ -258,7 +446,7 @@ mod tests {
     #[test]
     fn test_lex_delimiter_delimiter_in_between() {
         let source = "'as' 'df''";
-        let result = lex_delimiter(source, '\'');
+        let result = lex_char_delimited(source, '\'');
         let expected = "'as'";
         assert!(result.is_some());
         if let Some((token, _)) = result {
@@ -269,11 +457,65 @@ mod tests {
     #[test]
     fn test_lex_delimiter_delimiter_in_between_2() {
         let source = "'as'x'df''";
-        let result = lex_delimiter(source, '\'');
+        let result = lex_char_delimited(source, '\'');
         let expected = "'as'";
         assert!(result.is_some());
         if let Some((token, _)) = result {
             assert_eq!(token.value, expected);
         }
+    }
+
+    #[test]
+    fn test_lex_keyword() {
+        let source = "into";
+        let result = lex_keyword(source);
+        assert!(result.is_some());
+        if let Some((token, _)) = result {
+            assert_eq!(token.value, source);
+        }
+    }
+
+    #[test]
+    fn test_lex_keyword_2() {
+        let source = "selectasdf";
+        let expected = "select";
+        let result = lex_keyword(source);
+        assert!(result.is_some());
+        if let Some((token, _)) = result {
+            assert_eq!(token.value, expected);
+            assert_eq!(token.kind, TokenKind::Keyword(KeywordType::Select))
+        }
+    }
+
+    #[test]
+    fn test_lex_keyword_invalid() {
+        let source = "Asdf";
+        let result = lex_keyword(source);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_lex_keyword_space_beginning() {
+        let source = " select";
+        let result = lex_keyword(source);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_lex_symbol_neq() {
+        let source = "!=";
+        let result = lex_symbol(source);
+        assert!(result.is_some());
+        if let Some((token, _)) = result {
+            assert_eq!(token.value, source);
+            assert_eq!(token.kind, TokenKind::Symbol(SymbolType::Neq));
+        }
+    }
+
+    #[test]
+    fn test_lex_symbol_space_beginning() {
+        let source = " !=";
+        let result = lex_symbol(source);
+        assert!(result.is_none());
     }
 }
