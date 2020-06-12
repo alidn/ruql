59d1e3248ee90b27dd9db678e2cc413bd3b0c4d3
ali <anili@my.bcit.ca>
2020-06-12 01:12:42 -0700

    Added an error type

---
+++ src/error.rs
@@ -0,0 +1,27 @@
+use crate::lexer::Location;
+use std::fmt;
+
+pub struct Error {
+    repr: ErrorKind,
+    location: Location,
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("ERR!").field(&self.repr).field(&self.location).finish()
+    }
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, loc: Location) -> Error {
+        Error {
+            repr: kind,
+            location: loc,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    InvalidToken,
+}
