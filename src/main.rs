c1e357c557223911d850db68c80bb7341fe102cb
Ali <ali.nili128@gmail.com>
2020-08-26 13:06:08 -0700

    add examples

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,78 @@
 mod ast;
 mod cursor;
+mod database;
 mod lex_error;
 mod lexer;
-use crate::ast::Statement;
-use crate::lex_error::LexError;
+mod table;
+
+use crate::ast::Parsable;
+use crate::database::Database;
 use crate::lexer::lex;
+use crate::table::Memory;
 
-fn main() {
-    // let source = "select name from users where user_id = 1;
-    // and email = 'something@gmail.com'";
-
-    // let tokens = lex(source)?;
-
-    // for token in tokens {
-    //     println!("{:?}", token);
-    // }
-
-    let source = "insert into hello values (one, two, three )";
+fn lex_tokens_example() {
+    let source = "select name from users where user_id = 1;
+    and email = 'something@gmail.com'";
 
     let tokens = lex(source).unwrap();
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
+fn insert_statement_example() {
+    let source = "insert into hello values (\'one\', 2, three)";
+
+    let tokens = lex(source).unwrap();
+    tokens.iter().for_each(|t| println!("{:?}", t));
 
     let stmt = ast::InsertStatement::from_tokens(&tokens).unwrap();
 
     println!("{:#?}", stmt.unwrap());
+}
+
+fn select_statement_example() {
+    let source = "select something as somethingelse from sometable";
+
+    let tokens = lex(source).unwrap();
+    tokens.iter().for_each(|t| println!("{:?}", t));
+
+    let stmt = ast::SelectStatement::from_tokens(&tokens).unwrap();
+
+    println!("{:#?}", stmt.unwrap());
+}
+
+fn create_statement_example() {
+    let source = "create table my_table (id text , name text)";
+
+    let tokens = lex(source).unwrap();
+    tokens.iter().for_each(|t| println!("{:?}", t));
+
+    let stmt = ast::CreateStatement::from_tokens(&tokens).unwrap();
+
+    println!("{:#?}", stmt.unwrap());
+}
+
+fn main() {
+    let create_statement_source = "create table mytable (id text , name text)";
+    let create_tokens = lex(create_statement_source).unwrap();
+    let create_statement = ast::CreateStatement::from_tokens(&create_tokens).unwrap().unwrap();
+
+    let insert_statement_source = "insert into mytable values (\'one\' , \'two\' )";
+    let insert_tokens = lex(insert_statement_source).unwrap();
+    insert_tokens.iter().for_each(|t| println!("{:?}", t));
+    let insert_statement = ast::InsertStatement::from_tokens(&insert_tokens)
+        .unwrap()
+        .unwrap();
+
+    let select_statement_source = "select id from mytable";
+    let select_tokens = lex(select_statement_source).unwrap();
+    let select_statement = ast::SelectStatement::from_tokens(&select_tokens).unwrap().unwrap();
+
+    let mut memory = Memory::default();
+    memory.create_table(create_statement).unwrap();
+    memory.insert(insert_statement).unwrap();
+    let result = memory.select(select_statement).unwrap();
+    println!("{:#?}", result);
 }
